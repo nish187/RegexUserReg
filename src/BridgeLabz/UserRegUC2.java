373d9cb45706237a132ab869cde95252a0c310da
nisha <nishantsingh187@gmail.com>
2022-12-02 23:57:09 +0530

    User Registration UC2

---
+++ src/BridgeLabz/UserRegUC2.java
@@ -0,0 +1,43 @@
+package BridgeLabz;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegUC2 {
+    public static void main(String[] args) {
+        UserRegUC2 userRegistration = new UserRegUC2();
+        userRegistration.validFirstName();
+        userRegistration.validSecondName();
+    }
+
+    public void validFirstName() {
+        Pattern pattern = Pattern.compile("^[A-Z]+[a-zA-Z]{2,}[0-9]*$");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter user firstname");
+        String firstName = scanner.next();
+
+        Matcher matcher = pattern.matcher(firstName);
+        if (matcher.matches()) {
+            System.out.println("First name is valid");
+        } else {
+            System.out.println("First name is Invalid");
+        }
+    }
+
+    public void validSecondName() {
+        Pattern pattern = Pattern.compile("^[A-Z]+[a-zA-Z]{2,}[0-9]*$");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter user second name");
+        String secondName = scanner.next();
+
+        Matcher matcher = pattern.matcher(secondName);
+        if (matcher.matches()) {
+            System.out.println("Second name is valid");
+        } else {
+            System.out.println("Second name is Invalid");
+        }
+    }
+}
